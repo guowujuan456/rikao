595671ddbb9aab90b5536706a302c601e01a0d5c
guowujuan <2683022357@qq.com>
2019-09-04 14:36:28 +0800

    arr数组

--- day4/src/views/Home.vue
+++ day4/src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+
+    <ul v-for="(item,index) in Arr" :key="index" @click="run">
+      <li>{{item}}</li>
+    </ul>
+    <span>1231</span>
+<!-- （2）可以进行数据的遍历（20分）
+（3）提交git
+（4）使用git进行代码管理（20分）
+（5）创建出自己的分支（20分） -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
+ data(){
+   return {
+     Arr:["a","2","5g","h"]
+   }
+ },
+ methods:{
+   run(){
+     this.Arr.forEach((item=>{
+       console.log(item)
+     }))
+   }
+ }
 }
 </script>
